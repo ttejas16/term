84e0fb9a360dde3dd056fb2f5825cfcc7a41d0e4
ttejas16 <jagtaptejas128@gmail.com>
2026-04-26 20:14:07 +0530

    feat: add bottom headers and basic colors

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::io;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
-    style::Stylize,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Style, Stylize},
     symbols::border,
     text::Line,
-    widgets::{Block, Padding, Widget},
+    widgets::{Block, Borders, Padding, Paragraph, Widget},
 };
 
 fn main() -> io::Result<()> {
@@ -22,7 +23,7 @@ pub struct App {
 impl Default for App {
     fn default() -> Self {
         App {
-            title: "  terminal  ",
+            title: " Terminal ",
             exit: false,
         }
     }
@@ -71,14 +72,56 @@ impl Widget for &App {
     where
         Self: Sized,
     {
+        let title_color = Color::Rgb(213, 202, 214);
         let title = Line::from(self.title.bold());
-        let status = Line::from("  [live]  ");
+        let status = Line::from(" [live] ");
 
         let block = Block::bordered()
-            .title(title.left_aligned())
+            .title(title.left_aligned().style(Style::default().bg(title_color)))
             .title(status.right_aligned())
+            // .padding(Padding::new(3, 3, 1, 1))
             .border_set(border::PLAIN);
 
+        let inner_area = block.inner(area);
+
+        let vertical_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Min(0),
+                Constraint::Length(4),
+                Constraint::Length(4),
+            ])
+            .split(inner_area);
+
+        let main_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Length(3),
+                Constraint::Length(1),
+                Constraint::Min(0),
+            ])
+            .split(vertical_chunks[0]);
+
         block.render(area, buf);
+
+        let border_top_block = Block::default()
+            .padding(Padding::new(2, 2, 1, 1))
+            .borders(Borders::TOP)
+            .border_style(Style::default().fg(Color::White));
+
+        let info_color = Color::Rgb(247, 181, 56);
+        let mint_color = Color::Rgb(239, 156, 218);
+
+        Paragraph::new("header").render(vertical_chunks[0], buf);
+        // Paragraph::new("processes").block(Block::default().borders(Borders::BOTTOM)).render(vertical_chunks[1], buf);
+        Paragraph::new("Total: 14.2 GB used / 32 GB     Processes: 312")
+            .block(border_top_block.clone())
+            .style(Style::default().fg(mint_color))
+            .render(vertical_chunks[1], buf);
+
+        Paragraph::new("[q]quit  [/]filter  [s]sort  [↑↓]scroll  [r]refresh ")
+            .block(border_top_block.clone())
+            .style(Style::default().fg(info_color))
+            .render(vertical_chunks[2], buf);
     }
 }
